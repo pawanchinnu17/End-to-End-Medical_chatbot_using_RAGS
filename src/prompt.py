4320b8b2ceae34f92b88df4ace30f31d4a4937e3
Gudivada pavan <116303925+pawanchinnu17@users.noreply.github.com>
2025-12-12 12:08:03 +0000

    updated files

---
+++ src/prompt.py
@@ -0,0 +1,16 @@
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+system_prompt = (
+    "You are an Medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+
